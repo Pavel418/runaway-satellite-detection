5cf23c101bd6c44d09e23641c7b5150dd4051cc5
Pavel Kuznetsov <62073659+Pavel418@users.noreply.github.com>
2024-08-31 23:20:57 +0400

    roboflow cropping

--- downloader.py
+++
@@ -1,7 +0,0 @@
-from sentinelhub import SHConfig
-
-config = SHConfig()
-
-if not config.sh_client_id or not config.sh_client_secret:
-    print("Warning! To use Process API, please provide the credentials (OAuth client ID and client secret).")
-
